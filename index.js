863ee584ba91ccf40b8dc75b492994d2f4e2a3a9
maxgunter99 <maxgunter99@gmail.com>
2019-05-08 16:28:54 -0500

    constant username and password authentication added, need to add crud functions to the username

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 const server = require('./server');
 
 server.listen(4000, () => {
-    console.log(`\n* Your Server is running on localhost 4000! *\n`);
+    console.log(`\n* Your Server is running on localhost http://localhost:4000 *\n`);
 });
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const server = express();
 
-//ROUTERS
-const userRouter = require('./data/helpers/userRouter');
-const postRouter = require('./data/helpers/postRouter');
+//ROUTER
+const postRouter = require('./data/postRouter');
 
 //MIDDLEWARE
 const helmet = require('helmet');
@@ -14,9 +13,53 @@ server.use(express.json());
 server.use(helmet());
 server.use(morgan('short'));
 
+//AUTOMATIC API URL EXTENSION
+server.use('/api/posts', restricted, username, postRouter)
+
 //SERVER HOMEPAGE
-server.get('/', (req, res, next) => {
+server.get('/', restricted, username, (req, res, next) => {
     res.send(`<h1>Server is alive</h1>`)
 });
+
+//ERROR HANDLER
+server.use(errorHandler)
+
+//AUTHORIZATION CHECK
+function restricted( req, res, next ) {
+    const password = req.headers.password;
+    if ( password === 'test' ) {
+        next();
+    } else {
+        res.status( 401 ).json({ message: 'Invalid Password Bro' })
+    }
+}
+
+//ONLY USERNAME
+// function only( name ) {
+//     return function(req, res, next ) {
+//         const usersName = req.headers.name
+//         if ( usersName.toLowerCase() === name.toLowerCase() ) {
+//             next();
+//         } else {
+//             res.status( 401 ).json({ message: 'Access Denied' })
+//         }
+//     }
+// }
+
+
+function username( req, res, next ) {
+        const username = req.headers.name;
+        if ( username === 'max' ) {
+            next();
+        } else if ( username !==' max' ) {
+            res.status( 401 ).json({ message: 'Invalid Username Bro ' })
+        }
+    // }
+}
+
+//ERROR HANDLER
+function errorHandler( error, req, res, next ) {
+    res.status( 400 ).json({ message: 'ERR_RR_EE=EEE-R', error })
+}
 
 module.exports = server;
